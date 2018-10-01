56a296fdfa418ee8b1204172c729ac3d572f1816
anticobalt <anticobalt@users.noreply.github.com>
2018-09-30 19:24:09 -0600

    Cleanup code

--- app/src/main/java/com/cmput301/assignment1/MainActivity.java
+++ app/src/main/java/com/cmput301/assignment1/MainActivity.java
@@ -21,13 +21,6 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<Log> logs = new ArrayList<>();
     private LogsAdapter adapter;
     private ListView logsView;
-
-    private ImageView mLoveEmoji;
-    private ImageView mAngerEmoji;
-    private ImageView mFearEmoji;
-    private ImageView mSurpriseEmoji;
-    private ImageView mJoyEmoji;
-    private ImageView mSadEmoji;
     private ArrayList<ImageView> emojis = new ArrayList<>();
 
     @Override
@@ -38,62 +31,30 @@ public class MainActivity extends AppCompatActivity {
         // Get the ListView that holds logs
         logsView = findViewById(R.id.logsListView);
 
-        // Collect emoji ImageViews
-        mLoveEmoji = findViewById(R.id.loveView);
-        mAngerEmoji = findViewById(R.id.angerView);
-        mFearEmoji = findViewById(R.id.fearView);
-        mSurpriseEmoji = findViewById(R.id.surpriseView);
-        mJoyEmoji = findViewById(R.id.joyView);
-        mSadEmoji = findViewById(R.id.sadView);
-        emojis.add(mLoveEmoji);
-        emojis.add(mAngerEmoji);
-        emojis.add(mFearEmoji);
-        emojis.add(mSurpriseEmoji);
-        emojis.add(mJoyEmoji);
-        emojis.add(mSadEmoji);
-
-        // Set click listener for each emoji
-        // Lack of `final` throws compile error
-        for (final ImageView emoji: emojis){
-            emoji.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v){
-
-                    // fetch comment from textbox
-                    TextInputEditText textbox = findViewById(R.id.textboxView);
-                    String comment = textbox.getText().toString();
-
-                    // clear it
-                    textbox.setText("");
-
-                    // For each emoji, manually determine associated Emotion
-                    //      then create Log instance with that Emotion.
-                    // Doing it this way avoids having to create unique listener for every emoji
-                    switch(emoji.getId()){
-                        case R.id.loveView: logs.add(new Log("love", new Date(), comment)); break;
-                        case R.id.fearView: logs.add(new Log("fear", new Date(), comment)); break;
-                        case R.id.angerView: logs.add(new Log("anger", new Date(), comment)); break;
-                        case R.id.surpriseView: logs.add(new Log("surprise", new Date(), comment)); break;
-                        case R.id.joyView: logs.add(new Log("joy", new Date(), comment)); break;
-                        case R.id.sadView: logs.add(new Log("sadness", new Date(), comment)); break;
-                    }
-
-                    adapter.notifyDataSetChanged();
-
-                }
-            });
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
         // Connect the log array with the logs view
         adapter = new LogsAdapter(this, R.layout.log, logs);
         logsView.setAdapter(adapter);
 
-        // Set click listener, then transfer Log data from MainActivity to EditActivity
+        // Collect emoji ImageViews
+        emojis.add((ImageView) findViewById(R.id.loveView));
+        emojis.add((ImageView) findViewById(R.id.angerView));
+        emojis.add((ImageView) findViewById(R.id.fearView));
+        emojis.add((ImageView) findViewById(R.id.surpriseView));
+        emojis.add((ImageView) findViewById(R.id.joyView));
+        emojis.add((ImageView) findViewById(R.id.sadView));
+
+        // Set click listener for each emoji
+        for (ImageView emoji: emojis){
+            setEmojiListener(emoji, adapter);
+        }
+
+        // Set click listener for logsView
+        setLogsListener(logsView);
+
+    }
+
+    private void setLogsListener(ListView logsView) {
+        // Set click listener, which transfers Log data from MainActivity to EditActivity
         //      via an Intent.
         // See https://developer.android.com/reference/android/widget/AdapterView.OnItemClickListener
         //      and user914425's answer on https://stackoverflow.com/a/7325248
@@ -112,6 +73,38 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
+
+    // Lack of `final` throws compile error
+    private void setEmojiListener(final ImageView emoji, final LogsAdapter adapter) {
+        emoji.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+
+                // fetch comment from textbox
+                TextInputEditText textbox = findViewById(R.id.textboxView);
+                String comment = textbox.getText().toString();
+
+                // clear it
+                textbox.setText("");
+
+                // For each emoji, manually determine associated Emotion
+                //      then create Log instance with that Emotion.
+                // Doing it this way avoids having to create unique listener for every emoji
+                switch(emoji.getId()){
+                    case R.id.loveView: logs.add(new Log("love", new Date(), comment)); break;
+                    case R.id.fearView: logs.add(new Log("fear", new Date(), comment)); break;
+                    case R.id.angerView: logs.add(new Log("anger", new Date(), comment)); break;
+                    case R.id.surpriseView: logs.add(new Log("surprise", new Date(), comment)); break;
+                    case R.id.joyView: logs.add(new Log("joy", new Date(), comment)); break;
+                    case R.id.sadView: logs.add(new Log("sadness", new Date(), comment)); break;
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+    }
+
+
 }
